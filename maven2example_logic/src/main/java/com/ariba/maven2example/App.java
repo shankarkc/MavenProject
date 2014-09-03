987a32ed833f8074c5117ca66ee8e24fca936cef
I306565 <s.kc@sap.com>
2014-09-03 17:48:24 +0530

    Initial mavern project

---
+++ maven2example_logic/src/main/java/com/ariba/maven2example/App.java
@@ -0,0 +1,16 @@
+package com.ariba.maven2example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+
+
+       public static String now()
+       {
+            return new java.util.Date().toString();  
+       }
+ 
+}
---
+++ maven2example_webapp/src/main/webapp/index.jsp
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<html>
+ <body>
+ <h2>Fancy Clock</h2>
+ <%= com.ariba.maven2example.App.now() %>
+ </body>
+</html>
