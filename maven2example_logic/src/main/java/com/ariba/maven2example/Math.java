ab7438226f321d27e08ee48f03fc63dad1d9fc5b
I306565 <s.kc@sap.com>
2014-09-04 14:52:13 +0530

    commit more tests

---
+++ maven2example_logic/src/main/java/com/ariba/maven2example/Math.java
@@ -0,0 +1,17 @@
+package com.ariba.maven2example;
+
+public class Math {
+	public int add(int a, int b)
+	{ 
+		return a+b;
+	}
+
+	public int subtract(int a, int b)
+	{ 
+		return a-b;
+	}
+	public int multiply(int a, int b)
+	{ 
+		return a*b;
+	}
+}
